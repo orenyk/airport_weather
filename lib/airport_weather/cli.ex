ef7ecace57007953ce8cd7025ad6e6ca1e4aa802
Oren Kanner <okanner@gmail.com>
2016-08-26 10:26:27 -0400

    Add CLI module with parse_args method

---
+++ lib/airport_weather/cli.ex
@@ -0,0 +1,32 @@
+defmodule AirportWeather.CLI do
+  
+  @moduledoc """
+  Handles the command line parsing of user input and the dispatch to the various
+  functions that look up the local weather at a given US airport and display it.
+  """
+
+  def main(argv) do
+    argv |> parse_args
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help. Otherwise it is an IATA
+  airport code, which is simply returned as the result.
+
+  ## Example
+      iex> AirportWeather.CLI.parse_args(["-h", "foo"])
+      :help
+      iex> AirportWeather.CLI.parse_args(["EWR"])
+      "EWR"
+  """
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv,
+                               switches: [help: :boolean],
+                               aliases: [h: :help])
+    case parse do
+      { [help: true], _, _ } -> :help
+      { _, [code], _ } -> code
+      _ -> :help
+    end
+  end
+end
---
+++ test/cli_test.exs
@@ -0,0 +1,20 @@
+defmodule CliTest do
+  use ExUnit.Case
+  doctest AirportWeather.CLI
+
+  import AirportWeather.CLI, only: [parse_args: 1]
+
+  test "parse_args returns :help if -h or --help are passed" do
+    assert parse_args(["-h", "foo"]) == :help
+    assert parse_args(["--help", "foo"]) == :help
+    assert parse_args(["foo", "-h"]) == :help
+  end
+
+  test "parse_args returns any other single input passed" do
+    assert parse_args(["foo"]) == "foo"
+  end
+
+  test "parse_args returns :help for any other input" do
+    assert parse_args(["foo", "bar"]) == :help
+  end
+end
